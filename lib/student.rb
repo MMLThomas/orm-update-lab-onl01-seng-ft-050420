90e16926369edfe24aa186b3bba727e7c9377333
michael thomas <michael.thomas2014@gmail.com>
2020-06-05 10:13:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,11 +22,18 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  # def self.drop_table
+  #   sql = <<-SQL
+  #     DROP TABLE students
+  #     SQL
+  #     DB[:conn].execute.(sql)
+  # end
+  
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students
       SQL
-      DB[:conn].execute.(sql)
+      DB[:conn].execute(sql)
   end
 
 end
